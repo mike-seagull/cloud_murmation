4c4ccf5523787e4b780d7525e62b82ef2bfa8afd
Mike Hollister <hollister.michael@gmail.com>
2021-08-15 04:42:26 +0000

    added oracle instances - oci

--- README.md
+++ README.md
@@ -3,11 +3,18 @@
 https://learn.hashicorp.com/tutorials/terraform/github-actions
 ### Terraform Cloud
 1. Create organization and workspace
-2. Create "sensative" variables for OCI secrets
-    - oci_tenacy
+2. Create "sensative" variables for OCI secrets:
+    - oci_tenancy
     - oci_user
-    - oci_fingerprint
     - oci_subnetid
-3. Create api token
+    - oci_fingerprint -> https://docs.oracle.com/en-us/iaas/Content/API/Concepts/apisigningkey.htm#four
+    - oci_api_private_key -> https://docs.oracle.com/en-us/iaas/Content/API/Concepts/apisigningkey.htm#two (use "pbcopy" to maintain file formatting)
+    - oci_ssh_public_key -> https://registry.terraform.io/providers/hashicorp/oci/latest/docs/resources/core_instance
+3. Create variables:
+    - oci_displayname1
+    - oci_hostname1
+    - oci_displayname2
+    - oci_hostname2
+4. Create api token
 ### Github
-4. Add api token "TF_API_TOKEN" as a "Repository Secret" to the repo
+5. Add api token "TF_API_TOKEN" as a "Repository Secret" to the repo
---
+++ oci.tf
@@ -0,0 +1,78 @@
+variable "oci_tenancy" {
+  type        = string
+  sensitive   = true
+}
+variable "oci_user" {
+  type        = string
+  sensitive   = true
+}
+variable "oci_subnetid" {
+  type        = string
+  sensitive   = true
+}
+variable "oci_fingerprint" {
+  type        = string
+  sensitive   = true
+}
+variable "oci_api_private_key" {
+  type        = string
+  sensitive   = true
+}
+variable "oci_ssh_public_key" {
+  type        = string
+  sensitive   = true
+}
+variable "oci_displayname1" {
+  type        = string
+}
+variable "oci_hostname1" {
+  type        = string
+}
+variable "oci_displayname2" {
+  type        = string
+}
+variable "oci_hostname2" {
+  type        = string
+}
+provider "oci" {
+  tenancy_ocid = var.oci_tenancy
+  user_ocid = var.oci_user
+  fingerprint = var.oci_fingerprint
+  private_key = var.oci_api_private_key
+  region = "us-phoenix-1"
+}
+
+resource "oci_core_instance" "instance1" {
+  availability_domain = "yuqr:PHX-AD-1"
+  shape = "VM.Standard.E2.1.Micro"
+  compartment_id = var.oci_tenancy
+  create_vnic_details {
+      subnet_id = var.oci_subnetid
+      hostname_label = var.oci_hostname1
+  }
+  display_name = var.oci_displayname1
+  metadata = {
+      ssh_authorized_keys = var.oci_ssh_public_key
+  }
+  source_details {
+      source_type = "image"
+      source_id = "ocid1.image.oc1.phx.aaaaaaaaky4luenz7yvuzz26zipiun6dzbkm7hkon7tppynpm2l6p32aen7a"
+  }
+}
+resource "oci_core_instance" "instance2" {
+  availability_domain = "yuqr:PHX-AD-1"
+  shape = "VM.Standard.E2.1.Micro"
+  compartment_id = var.oci_tenancy
+  create_vnic_details {
+      subnet_id = var.oci_subnetid
+      hostname_label = var.oci_hostname2
+  }
+  display_name = var.oci_displayname2
+  metadata = {
+      ssh_authorized_keys = var.oci_ssh_public_key
+  }
+  source_details {
+      source_type = "image"
+      source_id = "ocid1.image.oc1.phx.aaaaaaaaky4luenz7yvuzz26zipiun6dzbkm7hkon7tppynpm2l6p32aen7a"
+  }
+}

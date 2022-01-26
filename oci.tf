c8b85dca283ac072dc9cc0fc3de35995375fb153
Mike Hollister <hollister.michael@gmail.com>
2022-01-26 03:10:03 +0000

    added oci arm instance

--- oci.tf
+++ oci.tf
@@ -34,6 +34,12 @@ variable "oci_displayname2" {
 variable "oci_hostname2" {
   type        = string
 }
+variable "oci_displayname3" {
+  type        = string
+}
+variable "oci_hostname3" {
+  type        = string
+}
 provider "oci" {
   tenancy_ocid = var.oci_tenancy
   user_ocid = var.oci_user
@@ -68,6 +74,27 @@ resource "oci_core_instance" "oci_instance2" {
       hostname_label = var.oci_hostname2
   }
   display_name = var.oci_displayname2
+  metadata = {
+      ssh_authorized_keys = var.oci_ssh_public_key
+  }
+  source_details {
+      source_type = "image"
+      source_id = "ocid1.image.oc1.phx.aaaaaaaaky4luenz7yvuzz26zipiun6dzbkm7hkon7tppynpm2l6p32aen7a"
+  }
+}
+resource "oci_core_instance" "oci_instance3" {
+  availability_domain = "yuqr:PHX-AD-1"
+  shape = "VM.Standard.A1.Flex"
+  compartment_id = var.oci_tenancy
+  create_vnic_details {
+      subnet_id = var.oci_subnetid
+      hostname_label = var.oci_hostname3
+  }
+  shape_config {
+      memory_in_gbs = 24
+      ocpus = 4
+  }
+  display_name = var.oci_displayname3
   metadata = {
       ssh_authorized_keys = var.oci_ssh_public_key
   }
